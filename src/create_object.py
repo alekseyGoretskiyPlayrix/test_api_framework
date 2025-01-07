7be5c3be8f17afa36c0f65b8c516f6c47f32b0e5
Aleksey Goretskiy <aleksey.goretskiy@playrix.com>
2025-01-07 16:36:34 +0100

    add basic classes

---
+++ src/create_object.py
@@ -0,0 +1,20 @@
+from typing import Any
+
+import requests
+
+
+class CreateObject:
+    response: Any
+
+    payload = {
+        "name": "Apple MacBook Pro 16",
+        "data": {
+            "year": 2019,
+            "price": 1849.99,
+            "CPU model": "Intel Core i9",
+            "Hard disk size": "1 TB"
+        }
+    }
+
+    def create_object(self):
+        response = requests.post("https://api.restful-api.dev/objects", data=self.payload)
