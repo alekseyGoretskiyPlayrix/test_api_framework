a2d0287883dbe6bc9d498566dda68b5431eccd93
Aleksey Goretskiy <aleksey.goretskiy@playrix.com>
2025-01-07 22:15:50 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
+# Base URLs for the REST API
+API_URL_OBJECT_CREATION = "https://api.restful-api.dev/objects"
 
+# Default payloads
+PAYLOAD_OBJECT_CREATION = {
+    "name": "Apple MacBook Pro 16",
+    "data": {
+        "year": 2019,
+        "price": 1849.99,
+        "CPU model": "Intel Core i9",
+        "Hard disk size": "1 TB"
+    }
+}
