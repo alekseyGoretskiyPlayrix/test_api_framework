23d9dfc99a41fad81ed52589746b811afaed60d9
Aleksey Goretskiy <aleksey.goretskiy@playrix.com>
2025-01-07 22:49:24 +0100

    Create update_object.py

---
+++ src/update_object.py
@@ -0,0 +1,13 @@
+from typing import Any
+
+import requests
+
+from config import PAYLOAD_OBJECT_UPDATE, API_URL_OBJECT_7
+
+
+class UpdateObject:
+    response: Any
+
+    def update_object(self):
+        self.response = requests.put(API_URL_OBJECT_7, json=PAYLOAD_OBJECT_UPDATE)
+        return self.response.json()
